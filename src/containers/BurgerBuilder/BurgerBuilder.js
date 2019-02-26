0ddf66b8d595ef7192135ce23db27e0760354e3f
Galiant <antonijodev@gmail.com>
2019-02-26 21:30:32 +0000

    Calculating the ingredient sum dynamically

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,22 +3,22 @@ import Auxi from '../../hoc/Auxi';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {...}
-  // }
-  state={
-    ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
-    }
-  }
+	// constructor(props) {
+	//   super(props);
+	//   this.state = {...}
+	// }
+	state = {
+		ingredients: {
+			salad: 0,
+			bacon: 0,
+			cheese: 0,
+			meat: 0,
+		},
+	};
 	render() {
 		return (
 			<Auxi>
-				<Burger ingredients={this.state.ingredients}/>
+				<Burger ingredients={this.state.ingredients} />
 				<div>Build Controls</div>
 			</Auxi>
 		);
