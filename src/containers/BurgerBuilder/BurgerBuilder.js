0f225acb058f3e3676f210d4df8b86e9217ade51
Galiant <antonijodev@gmail.com>
2019-04-07 00:03:13 +0100

    Navigating to the checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -29,6 +29,7 @@ class BurgerBuilder extends Component {
 	};
 
 	componentDidMount() {
+		console.log(this.props);
 		axios
 			.get('https://react-burger-builder-ms.firebaseio.com/ingredients.json')
 			.then((response) => {
@@ -91,29 +92,30 @@ class BurgerBuilder extends Component {
 
 	purchaseContinueHandler = () => {
 		// alert('You Continue!');
-		this.setState({ loading: true });
-		const order = {
-			ingredients: this.state.ingredients,
-			price: this.state.totalPrice,
-			customer: {
-				name: 'Antonijo Galic',
-				address: {
-					street: 'My Street 1',
-					zipCode: '85831',
-					country: 'Ireland',
-				},
-				email: 'test@test.com',
-			},
-			deliveryMethod: 'fastest',
-		};
-		axios
-			.post('/orders.json', order)
-			.then((response) => {
-				this.setState({ loading: false, purchasing: false });
-			})
-			.catch((error) => {
-				this.setState({ loading: false, purchasing: false });
-			});
+		// this.setState({ loading: true });
+		// const order = {
+		// 	ingredients: this.state.ingredients,
+		// 	price: this.state.totalPrice,
+		// 	customer: {
+		// 		name: 'Antonijo Galic',
+		// 		address: {
+		// 			street: 'My Street 1',
+		// 			zipCode: '85831',
+		// 			country: 'Ireland',
+		// 		},
+		// 		email: 'test@test.com',
+		// 	},
+		// 	deliveryMethod: 'fastest',
+		// };
+		// axios
+		// 	.post('/orders.json', order)
+		// 	.then((response) => {
+		// 		this.setState({ loading: false, purchasing: false });
+		// 	})
+		// 	.catch((error) => {
+		// 		this.setState({ loading: false, purchasing: false });
+		// 	});
+		this.props.history.push('/checkout');
 	};
 
 	render() {
